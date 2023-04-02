6fe5c9f83237df72933e415db8123c98aac9dd38
Mustafa Siddiqui <mustafa.eins@gmail.com>
2023-04-02 17:09:58 -0400

    Define class

--- observer-pattern/statistics-display.h
+++ observer-pattern/statistics-display.h
@@ -6,3 +6,36 @@
  * @copyright Copyright (c) 2023
  *
  */
+
+#include "interfaces/display-element-intf.h"
+#include "interfaces/observer-intf.h"
+#include <vector>
+
+class StatisticsDisplay : public Observer, public DisplayElement {
+  protected:
+    std::vector<int> my_last_ten_temperature_measurements;
+
+  public:
+    /// Constructor
+    StatisticsDisplay() = default;
+
+    /**
+     * @brief Add temperature measurement value to list of stored measurements.
+     *
+     * @param temperatureMeasurement temperature value
+     * @return null
+     */
+    void addMeasurement(int temperatureMeasurement);
+
+    /// Interface methods
+
+    void update();
+
+    /**
+     * @brief Display the average, min, & max temperature measurements.
+     *
+     * @param null
+     * @return null
+     */
+    void display();
+};
