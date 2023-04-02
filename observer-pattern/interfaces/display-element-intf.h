ab4e3693526f1fb051faee290122757cad2fc040
Mustafa Siddiqui <mustafa.eins@gmail.com>
2023-04-02 15:49:37 -0400

    Add display element interface file

---
+++ observer-pattern/interfaces/display-element-intf.h
@@ -0,0 +1,20 @@
+/**
+ * @file    display-element-intf.h
+ * @author  Mustafa Siddiqui
+ * @brief   Definition of the DisplayElement interface.
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+/**
+ * @brief Interface for a display element class.
+ */
+class DisplayElement {
+    /**
+     * @brief Display element by rendering correct info.
+     * @param null
+     * @return null
+     */
+    void display();
+};
