17aabe726f4698f59d54abc2f3ffa4a0d55e0e15
Mustafa Siddiqui <mustafa.eins@gmail.com>
2023-04-02 17:14:40 -0400

    Define class

--- observer-pattern/forecast-display.h
+++ observer-pattern/forecast-display.h
@@ -6,3 +6,22 @@
  * @copyright Copyright (c) 2023
  *
  */
+
+#include "interfaces/display-element-intf.h"
+#include "interfaces/observer-intf.h"
+
+class ForecastDisplay : public Observer, public DisplayElement {
+  protected:
+  public:
+    /// Constructor
+    ForecastDisplay() = default;
+
+    /// Interface methods
+
+    void update();
+
+    /**
+     * @brief Display shows the weather forecast based on the barometer.
+     */
+    void display();
+};
