ecf3ba277fa2556c638aeec948184661058cc08e
Mustafa Siddiqui <mustafa.eins@gmail.com>
2023-09-17 02:51:35 -0400

    Implement use of decorator

--- decorator-pattern/main.cc
+++ decorator-pattern/main.cc
@@ -7,11 +7,15 @@
  *
  */
 
+#include "enums/condiment-type.h"
 #include "enums/muffin-type.h"
 #include "enums/sandwich-type.h"
+#include "menu-items/condiments/condiment.h"
 #include "menu-items/muffin/muffin.h"
 #include "menu-items/sandwich/sandwich.h"
 #include <iostream>
+#include <memory>
+#include <vector>
 
 int main(void) {
 
@@ -26,6 +30,39 @@ int main(void) {
 
     std::cout << chocolateChipMuffin.toString() << std::endl;
     std::cout << blueberryMuffin.toString() << std::endl;
+
+    // Decorate sandwich with condiment
+    auto chickenSandwichWithBbqSauce = std::make_shared<Condiment>(
+        CondimentType::BBQ_SAUCE, std::make_shared<Sandwich>(chickenSandwich));
+
+    std::cout << chickenSandwichWithBbqSauce->toString() << std::endl;
+
+    auto grilledCheeseSandwichWithKetchup = std::make_shared<Condiment>(
+        CondimentType::KETCHUP,
+        std::make_shared<Sandwich>(grilledCheeseSandwich));
+
+    std::cout << grilledCheeseSandwichWithKetchup->toString() << std::endl;
+
+    // Store all sandwiches
+    std::vector<std::shared_ptr<SandwichInterface>> sandwiches = {
+        std::make_shared<Sandwich>(chickenSandwich),
+        std::make_shared<Sandwich>(grilledCheeseSandwich),
+        chickenSandwichWithBbqSauce, grilledCheeseSandwichWithKetchup};
+
+    // Print all sandwiches
+    std::cout << "=== Summary ===" << std::endl;
+    for (auto sandwich : sandwiches) {
+        std::cout << sandwich->toString() << std::endl;
+    }
+
+    // Print total cost
+    std::cout << "=== Total ===" << std::endl;
+    double cost = 0.0;
+    for (auto sandwich : sandwiches) {
+        cost += sandwich->getCost();
+    }
+    std::cout << "Number of Sandwiches: " << sandwiches.size() << std::endl;
+    std::cout << "Cost: $" << cost << std::endl;
 
     return 0;
 }
